7f5a9ef904969ff61bb37232cdd89d5e1d81293f
pateldev611 <devpatel0611@gmail.com>
2024-04-11 11:58:06 +0530

    public and server files added

---
+++ server/Middlewares/AuthMiddlewares.js
@@ -0,0 +1,27 @@
+
+const jwt = require("jsonwebtoken");
+const UserModel = require("../Models/AuthModel");
+
+module.exports.checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(
+      token,
+      "kishan sheth super secret key",
+      async (err, decodedToken) => {
+        if (err) {
+          res.json({ status: false });
+          next();
+        } else {
+          const user = await UserModel.findById(decodedToken.id);
+          if (user) res.json({ status: true, user: user.email });
+          else res.json({ status: false });
+          next();
+        }
+      }
+    );
+  } else {
+    res.json({ status: false });
+    next();
+  }
+};
